5df225f460be8ae0d58fd07499418bd2e63ef828
vemuez <Yussuf.Elarif@Harrogate.Jet2.com>
2017-05-10 10:47:49 +0100

    fix for the issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 //our root app component
-import {Component, OnInit, ChangeDetectorRef} from '@angular/core'
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -11,16 +11,16 @@ export class AppComponent implements OnInit {
   total: number = 100;
   collection = [];
   paginatedResults = [];
-  
-  dropDownList = [{id: 1, title: 'item', total: 100}, {id: 2, title: 'brand', total: 60}];
-  
+
+  dropDownList = [{ id: 1, title: 'item', total: 100 }, { id: 2, title: 'brand', total: 60 }];
+
   constructor(private changeDetectorRef: ChangeDetectorRef) { }
-  
+
   ngOnInit(): void {
     this.selected(1);
     this.pageChanged()
   }
-  
+
   list(name: string, length: number): void {
     let collection = [];
     for (let i = 1; i <= length; i++) {
@@ -28,21 +28,23 @@ export class AppComponent implements OnInit {
     }
     this.collection = collection;
   }
-  
+
   pageChanged(page?: number): void {
-    if (page){ this.currentPage = page; }
+    if (page) { this.currentPage = page; }
     let end = this.itemsPerPage * this.currentPage;
     let start = end - this.itemsPerPage;
     this.paginatedResults = this.collection.slice(start, end);
     this.changeDetectorRef.detectChanges();
   }
-  
+
   selected(id: number): void {
     let option = this.dropDownList.find(x => x.id == id);
     this.list(option.title, option.total);
     this.total = this.collection.length;
-    this.currentPage = 1;
-    // setTimeout(() => this.currentPage = 1, 50);
   }
-  
+
+  changePage(page: number): void {
+    this.currentPage = 1;
+  }
+
 }
